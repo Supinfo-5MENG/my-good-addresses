f8e62ea82fae7963f6a4361c68044c99750b7cd2
Ewennn <ewen.bosquet@gmail.com>
2025-10-27 21:46:24 +0100

    feat/ Define environment variables for firebase config

--- services/firebase/firebase.ts
+++ services/firebase/firebase.ts
@@ -2,26 +2,15 @@ import { getApp, getApps, initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
-// Config Ewen
-// const firebaseConfig = {
-//     apiKey: "AIzaSyDj2QQEGeBRWx-dz5Sm-JIe6n0W_evKIko",
-//     authDomain: "my-good-addresses-d7bf8.firebaseapp.com",
-//     projectId: "my-good-addresses-d7bf8",
-//     storageBucket: "my-good-addresses-d7bf8.firebasestorage.app",
-//     messagingSenderId: "950097850723",
-//     appId: "1:950097850723:web:f7df494a93a7463c8ba356"
-// };
-
-// Config Clement
 const firebaseConfig = {
-    apiKey: "AIzaSyAXddfzeeu11C1K4UUm-qkSz5VrCk4949I",
-    authDomain: "my-good-adresses.firebaseapp.com",
-    projectId: "my-good-adresses",
-    storageBucket: "my-good-adresses.firebasestorage.app",
-    messagingSenderId: "116853918771",
-    appId: "1:116853918771:web:47d073c0e4b67271a7a746",
-    measurementId: "G-V9YW0RLW0V"
-};
+    apiKey: process.env.EXPO_PUBLIC_API_KEY,
+    authDomain: process.env.EXPO_PUBLIC_AUTH_DOMAIN,
+    projectId: process.env.EXPO_PUBLIC_PROJECT_ID,
+    storageBucket: process.env.EXPO_PUBLIC_STORAGE_BUCKET,
+    messagingSenderId: process.env.EXPO_PUBLIC_MESSAGING_SENDER_ID,
+    appId: process.env.EXPO_PUBLIC_APP_ID,
+    measurementId: process.env.EXPO_PUBLIC_MEASUREMENT_ID,
+}
 
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
 
