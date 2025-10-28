49877ab5f5bb5c7bf47077b28b9473fc1a392ed8
Ewennn <ewen.bosquet@gmail.com>
2025-10-28 18:44:40 +0100

    fix/ Rollback hard coded env variables

--- services/firebase/firebase.ts
+++ services/firebase/firebase.ts
@@ -3,13 +3,13 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAXddfzeeu11C1K4UUm-qkSz5VrCk4949I",
-    authDomain: "my-good-adresses.firebaseapp.com",
-    projectId: "my-good-adresses",
-    storageBucket: "my-good-adresses.firebasestorage.app",
-    messagingSenderId: "116853918771",
-    appId: "1:116853918771:web:47d073c0e4b67271a7a746",
-    measurementId: "G-V9YW0RLW0V"
+    apiKey: process.env.EXPO_PUBLIC_API_KEY,
+    authDomain: process.env.EXPO_PUBLIC_AUTH_DOMAIN,
+    projectId: process.env.EXPO_PUBLIC_PROJECT_ID,
+    storageBucket: process.env.EXPO_PUBLIC_STORAGE_BUCKET,
+    messagingSenderId: process.env.EXPO_PUBLIC_MESSAGING_SENDER_ID,
+    appId: process.env.EXPO_PUBLIC_APP_ID,
+    measurementId: process.env.EXPO_PUBLIC_MEASUREMENT_ID,
 }
 
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
